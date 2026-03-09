b30faf93331aef64bd672aef9e30b2a6bb30f870
Joao Felipe Vicente <161844290+lfywho@users.noreply.github.com>
2026-03-09 08:10:23 -0300

    headerr

--- app/view/header.php
+++ app/view/header.php
@@ -1,5 +1,22 @@
 <header class="cabecalho">
 
+    <div class="logo">
+        <a href=""><img src="/../assets/imgs/logo.png" alt=""></a>
+    </div>
 
+    <nav class="navegarPaginas">
+        <div class="abaAtiva"><a class="" href="">Explorar</a></div>
+        <a href="">Aprender</a>
+        <a href="">Vagas</a>
+    </nav>
+    
+    <input class="inputPesquisa" type="search">
+
+    <div class="menusHeader">
+        <img src="/../assets/imgs/mail.png" alt="">
+        <img src="/../assets/imgs/bell.png" alt="">
+        <img src="/../assets/imgs/userProfile.png" alt="">
+        <img src="/../assets/imgs/options.png" alt="">
+    </div>
 
 </header>
